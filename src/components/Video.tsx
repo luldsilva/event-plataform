4d04f96c37f6151dd9877ca19daf956740e5774c
Lucas Miha <lucaslisilva@outlook.com>
2022-06-23 09:26:12 -0300

    add new buttons

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,34 @@
+import { DiscordLogo, Lightning } from "phosphor-react";
+
 export function Video() {
   return (
     <div className="flex-1">
-      Video
+      <div className="bg-black flex justify-center">
+          <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+      </div>
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font bold">
+              Aula 02 - Abertura
+            </h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Descricao de teste para visualizar o layout e validar as alteracoes.
+            </p>
+            <div></div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <a className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+            <a className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
