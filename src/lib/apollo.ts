39d58e5693146deda937e87cdfeb4a53bbfa7426
Lucas Miha <lucaslisilva@outlook.com>
2022-06-24 14:08:36 -0300

    add login page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4ppbcx42c4a01xlhr4a9wyh/master",
-  cache: new InMemoryCache()
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
+  cache: new InMemoryCache(),
 });
