04b194b637e1ffdfbd30d557776108ae9a3bee68
Lucas Miha <lucaslisilva@outlook.com>
2022-06-25 10:57:51 -0300

    add graph codegen in project and refactored querys and mutations

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,21 +1,14 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react"
 import { Navigate, useNavigate } from "react-router-dom";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
-const CREATE_SUBSCRIBER_MUTATION = gql`
-mutation CreateSubscriber($name: String!, $email: String!) {
-  createSubscriber(data: {name: $name, email: $email}) {
-    id
-  }
-}
-`
 
 export function Subscribe() {
   const navigate = useNavigate();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
   async function handleSubscriber(event: FormEvent) {
     event.preventDefault();
