015bb4382b60e3b8d6d31cf98e12a46d4623e0aa
Lucas Miha <lucaslisilva@outlook.com>
2022-06-27 09:53:41 -0300

    Fixed logo, adicioned blur on event page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <h1>Logo</h1>
+      <img src="/src/assets/logo.png"/>
    </header>
   )
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -5,7 +5,7 @@ import { Video } from "../components/Video"
 export function Event() {
   const { slug } = useParams<{slug: string}>()
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-blur">
       <Header />
       <main className="flex flex-1">
         {slug ?
